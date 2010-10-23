c985ceb36944c7918a14e1a245a44c7289e6b3fe
Jared Mitchell <s1052150@arkaig.inf.ed.ac.uk>
2010-10-23 17:20:08 +0100

    Solid work

--- scripts/pic_stuff.m
+++ scripts/pic_stuff.m
@@ -1,64 +1,42 @@
-first_hist = dohist('maze.jpg',0);
-nomaze_pic = myjpgload('nomaze.jpg',0);
-maze_pic = myjpgload('maze.jpg',0);
-thresh = findthresh(first_hist,4,0);
-mazet_pic = maze_pic > thresh;
-nomazet_pic = nomaze_pic > thresh;
-mazet_pic = im2bw(maze_pic);
-nomazet_pic = im2bw(nomaze_pic);
-diff_pic = ~(mazet_pic | nomazet_pic) + mazet_pic;
+diff_pic = removeBackground( 'maze.jpg', 'nomaze.jpg');
+
+imwrite(diff_pic, 'diff.jpg','jpg');
 
 [height, width] = size(diff_pic);
 
-window_size = 3;
-black_a_width = floor(width / window_size);
-black_a_height = floor(height / window_size);
-black_area = zeros(black_a_height, black_a_width);
-
-for i = (1:black_a_height - 1) * window_size + 1
-    for j = (1:black_a_width -1) * window_size + 1
-
-         tiny = diff_pic(i - (window_size - 1)/2: i + (window_size - 1)/2, ... 
-            j - (window_size - 1)/2: j + (window_size - 1)/2); 
-            
-         black_pics = window_size ^ 2 - sum(sum(tiny));
-         black_area(floor(i / window_size), floor(j / window_size)) =  ...
-            (black_pics >  0.8 * window_size ^ 2);
-    end
-
-end
-
-% looking for corner dots:
-%
 % find compact areas
 [labeled, labels_no] = bwlabel(diff_pic * (-1) + 1, 4);
-% compute compactness
-compactness = zeros(labels_no, 1);
-for label = (1:labels_no)
-    [r, c] = find(labeled == label);
-    [h, w] = size(labeled);
-    compact_window = max(max(r) - min(r), max(c) - min(c));
-    compact_area = labeled( min(r) : min(min(r) + compact_window, h), ...
-        min(c) : min(min(c) + compact_window, w)) == label;
-    compactness(label) = getcompactness(compact_area);
-end
-% choose those areas which are resemble circle
-CIRCLE_TRESHOLD = 0.2;
-circles_labels = find(abs(compactness - 1) < CIRCLE_TRESHOLD);
-imwrite(ismember(labeled, circles_labels), '1.jpg')
-[h,w] = size(circles_labels);
-centroids = regionprops(labeled, 'Centroid');
+compactness = findAreasCompactness(labeled, labels_no);
+circles = getCircles(labeled, compactness, 10);
+[corners, notcorners] = takeFourCorners(circles);
 
-X = zeros(h, 2);
-for i=(1:h)
-   s = centroids(circles_labels(i)).Centroid;
-   X(i,1) = s(1); X(i,2)=s(2);
-end
-circles_convhull = convhull(X(:,1), X(:,2));
+projected_pic = remap('diff.jpg', 'jpg', corners);
 
-corners = zeros(4,2);
-for i = (1 : 4)
-    point = centroids(circles_labels(circles_convhull(i))).Centroid;
-    corners(i, 1) = point(1);
-    corners(i, 2) = point(2);
+
+
+new_corners = [corners(:,2),corners(:, 1)];
+
+min = 10000;
+min_index = -1;
+for i = 1:4
+   
+    dist = distance(new_corners(i,:), notcorners(1,:));
+    
+    if dist < min
+        min = dist;
+        min_index = i;
+    end
 end
+
+%new_conrners(min_index, 1)
+
+robot_point = findBigSpots('mazewR.jpg', 'maze.jpg', corners, 1)
+
+osb1 = findBigSpots('mazewR.jpg', 'maze.jpg', corners, 2)
+obs2 = findBigSpots('mazewR.jpg', 'maze.jpg', corners, 3)
+%imshow(projwobs_pic);
+
+
+    
+
+
